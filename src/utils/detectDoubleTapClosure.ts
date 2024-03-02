1ef942c897572fe0fd46cff4a2e4d1a035c528d1
Lucas Fernando <lucasfernando.dev@gmail.com>
2024-03-02 02:32:06 -0300

    ✨ feat: Decreasing time between doble tapping on touch

--- src/utils/detectDoubleTapClosure.ts
+++ src/utils/detectDoubleTapClosure.ts
@@ -4,13 +4,13 @@ export function detectDoubleTapClosure(fn: () => void) {
   return function detectDoubleTap(event: TouchEvent) {
     const curTime = new Date().getTime();
     const tapLen = curTime - lastTap;
-    if (tapLen < 500 && tapLen > 0) {
+    if (tapLen < 350 && tapLen > 0) {
       fn();
       event.preventDefault();
     } else {
       timeout = setTimeout(() => {
         clearTimeout(timeout);
-      }, 500);
+      }, 350);
     }
     lastTap = curTime;
   };
